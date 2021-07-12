988ef131a67598f41f249f73eafd73b5c2f58992
Nancy Piedra <nancy_piedra@yahoo.com>
2021-07-12 11:51:57 -0400

    Remove keys

--- utilities/cognito.js
+++ utilities/cognito.js
@@ -2,9 +2,9 @@ const AWS = require('aws-sdk');
 
 function getCognitoIdentityServiceProvider() {
     var params = {
-        accessKeyId : "AKIAWW2F2QXXUDQU6O5N" /*process.env.AWS_ACCESS_KEY_ID*/,
-        secretAccessKey: "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"/*process.env.AWS_SECRET_ACCESS_KEY*/,
-        region: "us-east-1"/*process.env.AWS_REGION*/
+        accessKeyId : process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION
     };
     return new AWS.CognitoIdentityServiceProvider(params);
 }
@@ -27,18 +27,3 @@ exports.adminConfirmSignUp = function(params){
     return cognitoIdentifyServiceProvider.adminConfirmSignUp(params).promise();
 }
 
-exports.getUsers = function(){
-    console.log("getUsers()");
-    return new Promise(function(resolve, reject){
-        var params = {
-            UserPoolId: "us-east-1_kTtaNstIX"
-        };
-        module.exports.listUsers(params).then(function(result){
-            console.log(result);
-            resolve(result);
-        }).catch(function(err){
-            console.log(err);
-            reject(err);
-        });
-    });
-}
